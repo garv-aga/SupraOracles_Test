09ea28de003fb90fa4926671f67f23e74584103c
garv-aga <garvagarwal2003@gmail.com>
2024-01-15 00:18:37 +0530

    added token swap

--- src/TokenSwap.sol
+++ src/TokenSwap.sol
@@ -13,7 +13,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  *     ● Implement proper checks to ensure that the swap adheres to the exchange rate.
  *     ● Include events to log swap details.
  */
-contract TokenSwap {
+contract TokenSwap is ReentrancyGuard {
     address public owner;
     IERC20 public tokenA;
     IERC20 public tokenB;
@@ -28,16 +28,11 @@ contract TokenSwap {
         exchangeRate = _exchangeRate;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner");
-        _;
-    }
-
     /**
      * @dev Performs swap of A tokens for B Tokens
      * @param amountA Amount of token A to swap
      */
-    function swapAToB(uint256 amountA) external {
+    function swapAToB(uint256 amountA) external nonReentrant {
         uint256 amountB = amountA * exchangeRate;
         require(tokenA.balanceOf(msg.sender) >= amountA, "Insufficient balance of Token A");
         require(tokenB.balanceOf(address(this)) >= amountB, "Not enough Token B in the contract");
@@ -52,7 +47,7 @@ contract TokenSwap {
      * @dev Performs swap of B tokens for A Tokens
      * @param amountB Amount of token B to swap
      */
-    function swapBToA(uint256 amountB) external {
+    function swapBToA(uint256 amountB) external nonReentrant {
         uint256 amountA = amountB / exchangeRate;
         require(tokenB.balanceOf(msg.sender) >= amountB, "Insufficient balance of Token B");
         require(tokenA.balanceOf(address(this)) >= amountA, "Not enough Token A in the contract");
